0e63e65bc052064fc052e07c543d93c5716741c0
Matheus Alves <matheusalves@unifor.br>
2026-02-07 17:03:41 -0300

    :recycle: refactor: Db seed refactor

---
+++ src/main/resources/db/seeds/professors.sql
@@ -0,0 +1,13 @@
+-- ============================================
+-- PROFESSORES
+-- ============================================
+INSERT INTO professors (id, registration, name, email, title, department) VALUES 
+(nextval('professors_seq'), '2600101', 'Maria Iana Araujo', 'maria.iana@unifor.br', 'Doutor', 'Ciência da Computação'),
+(nextval('professors_seq'), '2600102', 'Lia Avelino', 'lia.avelino@unifor.br', 'Doutora', 'Engenharia de Software'),
+(nextval('professors_seq'), '2600103', 'Maria Clara', 'maria.clara@unifor.br', 'Mestre', 'Sistemas de Informação'),
+(nextval('professors_seq'), '2600104', 'Jorge Luiz', 'jorge.luiz@unifor.br', 'Especialista', 'Arquitetura de Computadores'),
+(nextval('professors_seq'), '2600105', 'Leticia Amb', 'leticia.amb@unifor.br', 'Doutora', 'Engenharia de Software'),
+(nextval('professors_seq'), '2600106', 'Julia Emilia', 'julia.emilia@unifor.br', 'Doutora', 'Ciência da Computação');
+(nextval('professors_seq'), '2600107', 'Ana Carvalho', 'ana.carvalho@unifor.br', 'Doutora', 'Ciência da Computação');
+(nextval('professors_seq'), '2600108', 'Agatha Ferreira', 'agatha.ferreira@unifor.br', 'Doutora', 'Ciência da Computação');
+(nextval('professors_seq'), '2600109', 'Jana Moreira', 'jana.moreira@unifor.br', 'Doutora', 'Ciência da Computação');
