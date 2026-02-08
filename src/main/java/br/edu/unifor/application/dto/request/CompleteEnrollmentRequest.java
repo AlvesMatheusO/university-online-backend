31df760aebf65b24ab09006c7ad853bc415d7e09
Matheus Alves <matheusalves@unifor.br>
2026-02-08 14:40:13 -0300

    :sparkles: feat(dto): complete enrollment request

---
+++ src/main/java/br/edu/unifor/application/dto/request/CompleteEnrollmentRequest.java
@@ -0,0 +1,20 @@
+package br.edu.unifor.application.dto.request;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+
+public class CompleteEnrollmentRequest {
+
+    @NotNull
+    @DecimalMin("0.0")
+    @DecimalMax("10.0")
+    public BigDecimal grade;
+
+    @NotNull
+    @DecimalMin("0.0")
+    @DecimalMax("100.0")
+    public BigDecimal attendance;
+}
